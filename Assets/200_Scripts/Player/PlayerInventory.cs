7e389d1ccce38e0f9ad868b48408366d863a9bf0
RemDrz <remiroucairol3221@gmail.com>
2023-05-12 10:40:55 +0200

    Amélioration PickUp
    Correction d'un bug qui empechait de collecter un objet si on était entré en collision avec plusieurs collectibles à la fois

--- Assets/200_Scripts/Player/PlayerInventory.cs
+++ Assets/200_Scripts/Player/PlayerInventory.cs
@@ -6,13 +6,20 @@ public class PlayerInventory : MonoBehaviour
 {
 
     public List<string> Inventory;
+
     private bool pickUpAllowed;
+
     private GameObject currentGameObject;
     private string CurrentObjectRef;
+
+    List<GameObject> objetsEnCollision;
 
     private void Start()
     {
         Inventory = new List<string>();
+
+        objetsEnCollision = new List<GameObject>();
+
     }
 
     private void Update()
@@ -20,11 +27,11 @@ public class PlayerInventory : MonoBehaviour
 
         //Ramasser quand on appuie sur E et qu'on est dans la zone de trigger
 
-        if (pickUpAllowed && Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            //ajouter à la liste de l'inventaire et detruire l'objet
-            Inventory.Add(CurrentObjectRef);
-            Destroy(currentGameObject);
+
+            CollectObjects();
+
         }
     }
 
@@ -32,11 +39,13 @@ public class PlayerInventory : MonoBehaviour
     {
         if (collision.CompareTag("Collectible"))
         {
-            pickUpAllowed = true;
+
+            objetsEnCollision.Add(collision.gameObject);
+
+
 
             //Obtenir les refs de l'objet sur lequel on est actuellement
             CurrentObjectRef = collision.gameObject.GetComponent<Collectibles>().propRef;
-            currentGameObject = collision.gameObject;
         }
     }
 
@@ -46,8 +55,29 @@ public class PlayerInventory : MonoBehaviour
         {
 
             //Empecher le pickup quand on est sur aucun collectible
-            pickUpAllowed = false;
+
+
+            objetsEnCollision.Remove(collision.gameObject);
+
         }
     }
+
+    void CollectObjects()
+    {
+        if (objetsEnCollision.Count > 0)
+        {
+            GameObject premierObjet = objetsEnCollision[0];
+            // Faites quelque chose avec le premier objet collectible (par exemple, le détruire)
+
+            //ajouter à la liste de l'inventaire et detruire l'objet
+            Destroy(premierObjet);
+            Inventory.Add(CurrentObjectRef);
+
+
+            // Retirez le premier objet de la liste
+            objetsEnCollision.Remove(premierObjet);
+        }
+    }
+
 
 }
