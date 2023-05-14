13e71a29c96b8dbb340cd656628aaf1683b4b0b5
RemDrz <remiroucairol3221@outlook.fr>
2023-05-14 20:01:41 +0200

    Début de mouvements de base + tags
    Ajout d'un début de sprint et de roulade, pas d'endurence (+ ajouts de tags pour le joueur et son sac)

--- Assets/200_Scripts/Player/PlayerMovement.cs
+++ Assets/200_Scripts/Player/PlayerMovement.cs
@@ -5,16 +5,101 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public float moveSpeed;
+    [Header("Keybinds")]
+    public KeyCode MoveUpKey = KeyCode.UpArrow;
+    public KeyCode MoveLeftKey = KeyCode.LeftArrow;
+    public KeyCode MoveRightKey = KeyCode.RightArrow;
+    public KeyCode MoveDownKey = KeyCode.DownArrow;
+
+    public KeyCode sprintKey = KeyCode.X;
+    public KeyCode rollKey = KeyCode.C;
+
+    [Header("Walk")]
+
+    private float moveSpeed;
+    public float WalkSpeed;
+
+
+    [Header("Sprint")]
+
+    public float SprintSpeed;
+
+    [Header("Roll")]
+
+    public float rollDuration;
+    public bool isRolling = false;
+    private Vector2 dashDirection;
+    public float dashSpeed;
+
+    [Header("General")]
+
     public Rigidbody2D rb;
+
+    public SpriteRenderer spriteRenderer;
+    public Color baseColor;
+    public Color currentColor;
+
     public Vector2 moveDirection;
 
+    [Header("Invincibility")]
+
+    public bool isInvincible = false;
+    public float invincibilityDuration;
+    public float invincibilityTimer;
+
+    [Header("MovementState")]
+
+    public MovementState State;
+    public enum MovementState
+    {
+        Walking,
+        Sprinting,
+        Rolling
+    }
+
+
+
+    private void StateHandler()
+    {
+        if(isRolling)
+        {
+            State = MovementState.Rolling;
+        }
+        else if(Input.GetKey(sprintKey))
+        {
+            State = MovementState.Sprinting;
+            moveSpeed = SprintSpeed;
+            spriteRenderer.color = Color.magenta;
+        }
+        else
+        {
+            State = MovementState.Walking;
+            moveSpeed = WalkSpeed;
+            spriteRenderer.color = baseColor;
+        }
+    }
+
+    private void Start()
+    {
+        baseColor = spriteRenderer.color;
+    }
 
     // Update is called once per frame
     void Update()
     {
         //processing inputs
         ProcessInputs();
+        StateHandler();
+
+        if (isInvincible)
+        {
+            invincibilityTimer += Time.deltaTime;
+            if (invincibilityTimer >= invincibilityDuration)
+            {
+                isInvincible = false;
+                invincibilityTimer = 0f;
+            }
+        }
     }
 
     private void FixedUpdate()
@@ -29,10 +114,66 @@ public class PlayerMovement : MonoBehaviour
         float moveY = Input.GetAxisRaw("Vertical");
 
         moveDirection = new Vector2(moveX, moveY).normalized;
+
+        if(Input.GetKeyDown(rollKey) && !isRolling)
+        {
+            dashDirection = GetDashDirection();
+            StartCoroutine(PerformRoll());
+        }
+
     }
+
+    private IEnumerator PerformRoll()
+    {
+        isRolling = true;
+
+        // Effectuer les actions nécessaires pour la roulade (animations, mouvements, etc.)
+        isInvincible = true;
+        spriteRenderer.color = Color.yellow;
+
+
+        yield return new WaitForSeconds(rollDuration);
+
+        isRolling = false;
+        dashDirection = Vector2.zero;
+        spriteRenderer.color = baseColor;
+    }
+
+    private Vector2 GetDashDirection()
+    {
+        Vector2 _direction = Vector2.zero;
+
+        if(Input.GetKey(MoveUpKey))
+        {
+            _direction += Vector2.up;
+        }
+        if (Input.GetKey(MoveLeftKey))
+        {
+            _direction += Vector2.left;
+        }
+        if (Input.GetKey(MoveDownKey))
+        {
+            _direction += Vector2.down;
+        }
+        if (Input.GetKey(MoveRightKey))
+        {
+            _direction += Vector2.right;
+        }
+
+        return _direction.normalized;
+
+    }
+
 
     private void Move()
     {
-         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        if (isRolling)
+        {
+            rb.velocity = dashDirection * dashSpeed;
+        }
+        else
+        {
+            rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        }   
     }
 }
