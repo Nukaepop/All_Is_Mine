c1bba07d5b0d8e91a4577317948a999d2fbefeca
RemDrz <remiroucairol3221@outlook.fr>
2023-05-19 15:31:06 +0200

    ronde
    l'enemy fait une ronde, la stop quand il detecte le joueur et la reprend quand il le perd de vue

--- Assets/200_Scripts/AI/EnemyAI.cs
+++ Assets/200_Scripts/AI/EnemyAI.cs
@@ -6,7 +6,14 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     public Transform player; // Référence au transform du joueur
+
     public float detectionRange = 10f; // Portée de détection du joueur
+
+    private bool isPatrolling;
+    public float patrolSpeed = 2f;
+
+    public List<Transform> waypoints; // Liste des waypoints pour la ronde
+    private int currentWaypointIndex = 0;
 
     private NavMeshAgent navMeshAgent;
 
@@ -16,6 +23,8 @@ public class EnemyAI : MonoBehaviour
         navMeshAgent = GetComponent<NavMeshAgent>(); // Récupérer le composant NavMeshAgent
         navMeshAgent.updateRotation = false;
         navMeshAgent.updateUpAxis = false;
+
+        StartPatrol();
     }
 
     private void Update()
@@ -23,11 +32,21 @@ public class EnemyAI : MonoBehaviour
         // Vérifier si le joueur est détecté
         if (DetectPlayer())
         {
-
+            StopPatrol();
             MoveTowardsPlayer();
 
             // Le joueur est détecté, ajoutez ici votre logique de comportement
             // lorsque le joueur est repéré, par exemple, attaquer ou suivre le joueur
+        }
+
+        else if (isPatrolling)
+        {
+            // Continuer la ronde
+            ContinuePatrol();
+        }
+        else
+        {
+            StartPatrol();
         }
     }
 
@@ -52,6 +71,36 @@ public class EnemyAI : MonoBehaviour
     {
         navMeshAgent.SetDestination(player.position); // Définir la destination du joueur pour le NavMeshAgent
         Debug.Log("IA en mouvement");
+    }
+
+    private void StartPatrol()
+    {
+        isPatrolling = true;
+        navMeshAgent.speed = patrolSpeed;
+
+        // Définir la destination initiale du premier waypoint
+        navMeshAgent.SetDestination(waypoints[currentWaypointIndex].position);
+        Debug.Log("IA en ronde");
+    }
+
+    private void StopPatrol()
+    {
+        isPatrolling = false;
+        navMeshAgent.ResetPath();
+        Debug.Log("IA a arrêté la ronde");
+    }
+
+    private void ContinuePatrol()
+    {
+        // Vérifier si l'IA a atteint sa destination actuelle
+        if (!navMeshAgent.pathPending && navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+        {
+            // Passer au waypoint suivant
+            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Count;
+
+            // Définir la destination du prochain waypoint
+            navMeshAgent.SetDestination(waypoints[currentWaypointIndex].position);
+        }
     }
 
 }
